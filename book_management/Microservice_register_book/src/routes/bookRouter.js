6464d9d02258383d501985265fe58f631ed74235
Marangeles19 <135676071+Marangeles19@users.noreply.github.com>
2025-06-22 08:58:06 -0500

    Delete book_management directory

--- book_management/Microservice_register_book/src/routes/bookRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { bookController } from "../controllers/bookController.js";
-
-const bookRouter = express.Router();
-
-bookRouter.post("/add", (req, res) => bookController.addBook(req, res));
-
-export default bookRouter;
